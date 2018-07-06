0dc69ce262c07aaae006037cf5e81bf9dde92cb6
STARGROUP\kevinwang6303 <kevinwang6303@gmail.com>
2018-07-06 17:34:21 +0800

    初步完成

--- src/app/pages/home/home.component.ts
+++ src/app/pages/home/home.component.ts
@@ -6,15 +6,51 @@ import {
   ElementRef
 } from '@angular/core';
 import { fromEvent, Observable } from 'rxjs';
+import { scrollIt } from 'src/app/@core/ts/scroll-to';
+import { trigger, transition, style, animate } from '@angular/animations';
 
 @Component({
   selector: 'app-home',
   templateUrl: './home.component.html',
-  styleUrls: ['./home.component.scss']
+  styleUrls: ['./home.component.scss'],
+  animations: [
+    trigger('flyInOut', [
+      transition('void => *', [
+        style({ transform: 'translateX(-100%)' }),
+        animate(500)
+      ]),
+      transition('* => void', [
+        animate(500, style({ transform: 'translateX(-100%)' }))
+      ])
+    ])
+  ]
 })
 export class HomeComponent implements OnInit, AfterViewInit {
+
   @ViewChild('video') video: ElementRef;
-  @ViewChild('elm') elm: ElementRef;
+  @ViewChild('menu') menuElm: ElementRef;
+  @ViewChild('about') about: ElementRef;
+  @ViewChild('experience') experience: ElementRef;
+  @ViewChild('skills') skills: ElementRef;
+  @ViewChild('trait') trait: ElementRef;
+
+  menuArray = [
+    {
+      name: 'ABOUT',
+      active: true
+    },
+    {
+      name: 'EXPERIENCE',
+      active: false
+    }, {
+      name: 'SKILLS',
+      active: false
+    }, {
+      name: 'TRAIT',
+      active: false
+    }
+  ];
+
   menuOpen = false;
   scrollEvent: Observable<any>;
   toDay = new Date();
@@ -22,14 +58,26 @@ export class HomeComponent implements OnInit, AfterViewInit {
   constructor() {
     this.scrollEvent = fromEvent(window, 'scroll');
     this.scrollEvent.subscribe(() => {
-      if (
-        this.elm.nativeElement.parentElement.getBoundingClientRect().top < 0
-      ) {
-        this.elm.nativeElement.classList.add('fixed');
+
+      let tempWidth = window.innerWidth;
+
+      if (tempWidth < 1064) {
+
+        this.setFixed(60);
       } else {
-        this.elm.nativeElement.classList.remove('fixed');
+
+        this.setFixed(0);
       }
+
     });
+  }
+
+  private setFixed(value) {
+    if (this.menuElm.nativeElement.parentElement.getBoundingClientRect().top < value) {
+      this.menuElm.nativeElement.classList.add('fixed');
+    } else {
+      this.menuElm.nativeElement.classList.remove('fixed');
+    }
   }
 
   ngOnInit() { }
@@ -37,4 +85,55 @@ export class HomeComponent implements OnInit, AfterViewInit {
   ngAfterViewInit() {
     this.video.nativeElement.muted = true;
   }
+
+  goMenu(index) {
+
+    this.menuArray.forEach(
+      (item) => {
+        item.active = false;
+      }
+    )
+    this.menuArray[index].active = true;
+    switch (index) {
+      case 0:
+        this.about.nativeElement.scrollIntoView({
+          block: 'start',
+          behavior: 'smooth'
+        });
+        break;
+      case 1:
+        this.experience.nativeElement.scrollIntoView({
+          block: 'start',
+          behavior: 'smooth'
+        });
+        break;
+      case 2:
+        this.skills.nativeElement.scrollIntoView({
+          block: 'start',
+          behavior: 'smooth'
+        });
+        break;
+      case 3:
+        this.trait.nativeElement.scrollIntoView({
+          block: 'start',
+          behavior: 'smooth'
+        });
+        break;
+
+      default:
+        break;
+    }
+    /*
+        let tempWidth = window.innerWidth;
+        if (tempWidth < 1064) {
+          scrollIt(window.pageYOffset + eleHeight - 110, 300, 'linear');
+        } else {
+          scrollIt(window.pageYOffset + eleHeight - 50, 300, 'linear');
+        }*/
+
+  }
+  goTop() {
+    window.scroll({ top: 0, left: 0, behavior: 'smooth' });
+  }
+
 }
